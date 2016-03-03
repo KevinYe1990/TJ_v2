d96e25bae45e3a679a243c8b4652c6e9f791da85
KevinYe1990 <yewenkai1990@gmail.com>
2016-03-03 18:16:49 +0800

    add feature extraction part

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-static bool readConfigFile(const char *cfgfilepath, const string &key, string &value);
 static bool printConfigFile();
 
 //global variables
@@ -52,40 +51,7 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-static bool readConfigFile(const char *cfgfilepath, const string &key, string &value){
-    /*
-     * parameter:
-                 cfgfilepath - configuration file path
-                         key - variable name in the configuration file
-                       value - value that corresponds to the key
-    */
-    fstream cfgFile;
-    cfgFile.open(cfgfilepath);
-    if(!cfgFile.is_open()){
-        cerr<<"Error happened while loading the configuration file!"<<endl<<endl;
-        return -1;
-    }
-    char tmp[1000];
-    while(!cfgFile.eof()){
-        cfgFile.getline(tmp,1000);
-        if(tmp[0]=='#')
-            break;
-        else{
-            string line(tmp);
-            size_t pos=line.find('=');
-            if(pos==string::npos){
-                cerr<<"Error happened while reading the configuration file!"<<endl<<endl;
-                return -1;
-            }
-            string tmpKey=line.substr(0,pos);
-            if(key==tmpKey){
-                value=line.substr(pos+1);
-                return 1;
-            }
-        }
-    }
-    return -1;
-}
+
 
 static bool printConfigFile(){
     cout<<"Configuration File Content:"<<endl;
